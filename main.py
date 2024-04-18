6fde1e5825cca1f05d57e45e03bf0cf0de48a676
Shravya Nandyala <shravya.nandyala@gmail.com>
2024-04-18 00:22:58 -0400

    It's not really working

--- main.py
+++ main.py
@@ -11,36 +11,35 @@ import torch
 from datasets import Dataset, load_dataset
 from transformers import Wav2Vec2ForCTC, Wav2Vec2ProcessorWithLM
 
+# Using the ported transformers version of the fairseq translation model for
+# a lighter weight
+from transformers import FSMTConfig, FSMTForConditionalGeneration, FSMTTokenizer
+
 # Set the logging level to WARNING to suppress INFO and DEBUG messages
 logging.basicConfig(level=logging.WARNING)
 
 TRANSCRIPT_MODEL_ID = "bond005/wav2vec2-large-ru-golos-with-lm"
 DATASET_ID = "bond005/sberdevices_golos_10h_crowd"
-SAMPLES = 5
+SAMPLES = 1
 
 nltk.download('punkt', quiet=True)
 
 processor = Wav2Vec2ProcessorWithLM.from_pretrained(TRANSCRIPT_MODEL_ID)
 transcript_model = Wav2Vec2ForCTC.from_pretrained(TRANSCRIPT_MODEL_ID)
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    ru2en = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.ru-en',
-                        checkpoint_file='model1.pt:model2.pt:model3.pt:model4.pt',
-                        tokenizer='moses', bpe='fastbpe')
-    # Disable dropout
-    ru2en.eval()
-
-# TRANSLATE_MODEL_ID = "facebook/nllb-200-distilled-600M"
-# tokenizer = AutoTokenizer.from_pretrained(TRANSLATE_MODEL_ID)
-# translate_model = AutoModelForSeq2SeqLM.from_pretrained(TRANSLATE_MODEL_ID)
+# Use the Russian to English translation model
+TRANSLATION_MODEL_ID = "facebook/wmt19-ru-en"
+tokenizer = FSMTTokenizer.from_pretrained(TRANSLATION_MODEL_ID)
+en2ru_tokenizer = FSMTTokenizer.from_pretrained("facebook/wmt19-en-ru")
+ru2en = FSMTForConditionalGeneration.from_pretrained(TRANSLATION_MODEL_ID)
+ru2en.eval()
 
 # Preprocessing the datasets.
 # We need to read the audio files as arrays
 def speech_file_to_inputs_fn(batch):
     speech_array, sr = librosa.load(batch['path'], sr=16_000)
     
-    # Split audio file into one minute chunks so model can process all data
+    # Split audio file into smaller chunks so model can process all data
     sample_len = 30 * sr
     counter = 0
     samples_array = []
@@ -107,35 +106,55 @@ def main():
                                 return_tensors="pt",
                                 padding=True)
                 logits = transcript_model(inputs.input_values,
-                                        attention_mask=inputs.attention_mask).logits
+                                          attention_mask=inputs.attention_mask).logits
                 tscript = processor.decode(logits=logits.numpy()[:][:][0]).text
                 transcript_chunks.append(tscript)
-
-                # To translate, first encode, then run model, then decode
-                # translation = []
-                # for t in transcription:
-                    # input_ids = tokenizer.encode(t, return_tensors="pt")
-                    # output_ids = translate_model.generate(input_ids, max_new_tokens=100)
-                    # translation.append(tokenizer.decode(output_ids[0], skip_special_tokens=True))
                     
             # Link together the transcribed chunks
             transcriptions.append(' '.join(transcript_chunks))
 
-            # Encode all the translated phrases
-            encoded_phrases = list(map(lambda x: ru2en.encode(x), transcript_chunks))
+            # Encode all the transcripted phrases
+            encoded_phrases = list(map(lambda x: tokenizer.encode(x, return_tensors="pt"),
+                                       transcript_chunks))
             
             translated = []
             alignment = []
-            for results in ru2en.generate(encoded_phrases):
-                # Pick top scoring translation
-                best_trans = results[0]
-                translated.append(ru2en.decode(best_trans['tokens']))
+
+            for phrase in encoded_phrases:
+                generated = ru2en.generate(phrase,
+                                           output_attentions=True,
+                                           return_dict_in_generate=True)
                 
-                # Attention matrix has rows of output tokens corresponding to attention
-                # weights for each source token (columns)
-                # So to find which input token affected the output token most, find
-                # argmax across rows
-                alignment.append(best_trans['attention'].argmax(dim=1))
+                print("LAYER 1:", len(generated.cross_attentions))
+                print("LAYER 2:", len(generated.cross_attentions[0]))
+                print("LAYER 3:", len(generated.cross_attentions[0][0]))
+                print("LAYER 4:", len(generated.cross_attentions[0][0][0]))
+                print("LAYER 5:", len(generated.cross_attentions[0][0][0][0]))
+                print("LAYER 6:", len(generated.cross_attentions[0][0][0][0][0]))
+
+                # Batch size is 1 so just grab the first element of output tokens
+                output = generated.sequences[0]
+                result = tokenizer.decode(output, skip_special_tokens=True)
+                translated.append(result)
+                # For each output token
+                for i, token in enumerate(output):
+                    # Attention information for the current token
+                    attention_info = generated.cross_attentions[i]
+                    attention = []
+                    # Next is attention heads
+                    for attn_head in attention_info:
+                        # Next is number of beams, want the top one
+                        beam = attn_head[0]
+                        # Next is the number of layers, want the last one
+                        last_layer = beam[-1]
+                        # Next is the batch size, always 1
+                        attention.append(last_layer[0])
+                    attention = np.asarray(attention).mean(axis=0)
+                    max_index = attention.argmax().item()
+                    input_word = en2ru_tokenizer.decode([phrase[0, max_index]], skip_special_tokens=True)
+                    output_word = tokenizer.decode([token], skip_special_tokens=True)
+                    print(input_word, output_word)
+                    alignment.append((input_word, output_word))
                     
             translations.append(' '.join(translated))
             alignments.append(alignment)
